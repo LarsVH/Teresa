0c3000beb27b4943e7459fba5e331d324e70e2d7
Jari Van M <vmjari@hotmail.com>
2016-02-26 21:02:14 +0100

    Update Main.java

--- teresa_base/src/main/java/Main.java
+++ teresa_base/src/main/java/Main.java
@@ -10,15 +10,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("system started");
         Input input = new Input();
-<<<<<<< HEAD
         String command = input.getCommand(false);
         if (command.equals("TERESA")) {
             Output.speak("yes, i am listening");
             command = input.getCommand(true);
         }
-=======
         String command = input.getCommand();
->>>>>>> origin/master
         if (command.contains("TERESA")) Output.speak(Action.doAction(command.substring(7)));
         else Output.speak(Action.doAction(command));
     }
